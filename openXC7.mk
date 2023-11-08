cd5e3ebbfc047c1b6970a001a4b8ac9d4709ccaf
Hans Baier <hansfbaier@gmail.com>
2023-11-08 08:33:19 +0700

    openXC7.mk: add variable for top verilog file

--- openXC7.mk
+++ openXC7.mk
@@ -10,6 +10,7 @@ PYPY3 ?= pypy3
 
 TOP ?= ${PROJECT}
 TOP_MODULE ?= ${TOP}
+TOP_VERILOG ?= ${TOP}.v
 
 PNR_DEBUG ?= # --verbose --debug
 
@@ -25,7 +26,7 @@ all: ${PROJECT}.bit
 program: ${PROJECT}.bit
 	openFPGALoader ${JTAG_LINK} --bitstream $<
 
-${PROJECT}.json: ${TOP}.v ${ADDITIONAL_SOURCES}
+${PROJECT}.json: ${TOP_VERILOG} ${ADDITIONAL_SOURCES}
 	yosys -p "synth_xilinx -flatten -abc9 ${SYNTH_OPTS} -arch xc7 -top ${TOP_MODULE}; write_json ${PROJECT}.json" $< ${ADDITIONAL_SOURCES}
 
 # The chip database only needs to be generated once
