7737797874689ba374214c6836c30d89302bcbcc
Hans Baier <foss@hans-baier.de>
2026-08-17 17:05:16 +0700

    update litex-ddr-arty-s7 to modern litex design

---
+++ litex-ddr-arty-s7/build-litex.sh
@@ -0,0 +1,1 @@
+./digilent_arty_s7.py --with-led-chaser --cpu-variant minimal --sys-clk-freq 60e6 --build # --integrated-main-ram-size 0x8000
---
+++ litex-ddr-arty-s7/digilent_arty_s7.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+
+#
+# This file is part of LiteX-Boards.
+#
+# Copyright (c) 2015-2019 Florent Kermarrec <florent@enjoy-digital.fr>,
+# Copyright (c) 2020 Staf Verhaegen <staf@fibraservi.eu>
+# SPDX-License-Identifier: BSD-2-Clause
+
+from migen import *
+from migen.genlib.resetsync import AsyncResetSynchronizer
+
+from litex.gen import *
+
+from litex_boards.platforms import digilent_arty_s7
+
+from litex.soc.cores.clock import *
+from litex.soc.integration.soc import *
+from litex.soc.integration.builder import *
+from litex.soc.cores.led import LedChaser
+
+from litedram.modules import MT41K128M16
+from litedram.phy import s7ddrphy
+
+# CRG ----------------------------------------------------------------------------------------------
+
+class _CRG(LiteXModule):
+    def __init__(self, platform, sys_clk_freq, with_ddr=True):
+        self.rst    = Signal()
+        self.cd_sys = ClockDomain()
+
+        # # #
+
+        if with_ddr:
+            # DDR3 SDRAM clocks (sys/sys2x/sys4x/sys4x_dqs/idelay) from a single PLL.
+            self.cd_sys2x     = ClockDomain()
+            self.cd_sys4x     = ClockDomain()
+            self.cd_sys4x_dqs = ClockDomain()
+            self.cd_idelay    = ClockDomain()
+
+            self.pll = pll = S7PLL(speedgrade=-1)
+            self.comb += pll.reset.eq(~platform.request("cpu_reset_n") | self.rst)
+            pll.register_clkin(platform.request("clk100"), 100e6)
+            pll.create_clkout(self.cd_sys,       sys_clk_freq)
+            pll.create_clkout(self.cd_sys2x,     2*sys_clk_freq)
+            pll.create_clkout(self.cd_sys4x,     4*sys_clk_freq)
+            pll.create_clkout(self.cd_sys4x_dqs, 4*sys_clk_freq, phase=90)
+            pll.create_clkout(self.cd_idelay,    200e6)
+            platform.add_false_path_constraints(self.cd_sys.clk, pll.clkin) # Ignore sys_clk to pll.clkin path created by SoC's rst.
+
+            self.idelayctrl = S7IDELAYCTRL(self.cd_idelay)
+        elif sys_clk_freq == 100e6:
+            # Minimal CRG (no DDR3, no PLL): buffer the 100MHz input clock directly.
+            clk100     = platform.request("clk100")
+            clk100_buf = Signal()
+            self.specials += Instance("IBUF", i_I=clk100, o_O=clk100_buf)
+            self.specials += Instance("BUFG", i_I=clk100_buf, o_O=self.cd_sys.clk)
+            self.specials += AsyncResetSynchronizer(self.cd_sys, ~platform.request("cpu_reset_n") | self.rst)
+        else:
+            # Minimal CRG (no DDR3) at a non-100MHz sys clock: single-clkout PLL.
+            self.pll = pll = S7PLL(speedgrade=-1)
+            self.comb += pll.reset.eq(~platform.request("cpu_reset_n") | self.rst)
+            pll.register_clkin(platform.request("clk100"), 100e6)
+            pll.create_clkout(self.cd_sys, sys_clk_freq)
+            platform.add_false_path_constraints(self.cd_sys.clk, pll.clkin)
+
+# BaseSoC ------------------------------------------------------------------------------------------
+
+class BaseSoC(SoCCore):
+    def __init__(self, variant="s7-50", sys_clk_freq=100e6,
+        with_spi_flash  = False,
+        with_led_chaser = True,
+        **kwargs):
+        platform = digilent_arty_s7.Platform(variant=variant)
+
+        # CRG --------------------------------------------------------------------------------------
+        self.crg = _CRG(platform, sys_clk_freq, with_ddr=not kwargs.get("integrated_main_ram_size", 0))
+
+        # SoCCore ----------------------------------------------------------------------------------
+        SoCCore.__init__(self, platform, sys_clk_freq, ident="LiteX SoC on Arty S7", **kwargs)
+
+        # DDR3 SDRAM -------------------------------------------------------------------------------
+        if not self.integrated_main_ram_size:
+            self.ddrphy = s7ddrphy.A7DDRPHY(platform.request("ddram"),
+                memtype        = "DDR3",
+                nphases        = 4,
+                sys_clk_freq   = sys_clk_freq)
+            self.add_sdram("sdram",
+                phy           = self.ddrphy,
+                module        = MT41K128M16(sys_clk_freq, "1:4"),
+                l2_cache_size = kwargs.get("l2_size", 8192)
+            )
+
+        # SPI Flash --------------------------------------------------------------------------------
+        if with_spi_flash:
+            from litespi.modules import S25FL128S
+            from litespi.opcodes import SpiNorFlashOpCodes as Codes
+            self.add_spi_flash(mode="4x", module=S25FL128S(Codes.READ_1_1_4), with_master=True)
+
+        # Leds -------------------------------------------------------------------------------------
+        if with_led_chaser:
+            self.leds = LedChaser(
+                pads         = platform.request_all("user_led"),
+                sys_clk_freq = sys_clk_freq)
+
+# Build --------------------------------------------------------------------------------------------
+
+def main():
+    from litex.build.parser import LiteXArgumentParser
+    parser = LiteXArgumentParser(platform=digilent_arty_s7.Platform, description="LiteX SoC on Arty S7.")
+    parser.add_target_argument("--with-led-chaser", action="store_true",       help="Enable Led Chaser.")
+    parser.add_target_argument("--variant",        default="s7-50",           help="Board variant (s7-50 or s7-25).")
+    parser.add_target_argument("--sys-clk-freq",   default=100e6, type=float, help="System clock frequency.")
+    parser.add_target_argument("--with-spi-flash", action="store_true",       help="Enable memory-mapped SPI flash.")
+    args = parser.parse_args()
+
+    soc = BaseSoC(
+        variant         = args.variant,
+        sys_clk_freq    = args.sys_clk_freq,
+        with_spi_flash  = args.with_spi_flash,
+        with_led_chaser = args.with_led_chaser,
+        **parser.soc_argdict
+    )
+    builder = Builder(soc, **parser.builder_argdict)
+    if args.build:
+        builder.build(**parser.toolchain_argdict)
+
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(builder.get_bitstream_filename(mode="sram"))
+
+if __name__ == "__main__":
+    main()
